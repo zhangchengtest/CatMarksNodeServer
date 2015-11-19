20964cb6030af5deaeec42d1be270728a81262ce
itwap <itwap@foxmail.com>
2015-11-19 15:51:12 +0800

    完善了注册和登录机制

--- local_modules/config/config.js
+++ local_modules/config/config.js
@@ -3,31 +3,111 @@ var config = {
     uri: "mongodb://localhost",
     port: "27017"
   },
-  responseResult: {
-    status200: {
-      code:2000,
+  usersRes: {
+    status1000: {
+      code: 2000,
       status: 200,
-      message: "success!"
+      message: "成功!"
     },
-    status201: {
-      code:2001,
+    status1001: {
+      code: 1001,
       status: 200,
-      message: "content is already exists !"
+      message: "用户名只允许数字和字母的组合!"
     },
-    status400: {
-      code:4000,
-      status: 400,
-      message: "falied!"
+    status1002: {
+      code: 1002,
+      status: 200,
+      message: "用户名长度应该介于5-10之间!"
     },
-    status404: {
-      code:4004,
+    status1004: {
+      code: 1004,
+      status: 200,
+      message: "密码长度应介于6-15之间!"
+    },
+    status1005: {
+      code: 1005,
+      status: 200,
+      message: "该用户名已被注册!"
+    },
+    status1006: {
+      code: 1006,
+      status: 200,
+      message: "该邮箱地址已被注册!"
+    },
+    status1007: {
+      code: 1007,
+      status: 200,
+      message: "密码错误!"
+    },
+    status1008: {
+      code: 1008,
       status: 404,
-      message: "content is not exists !"
+      message: "账号不存在!",
+      suggestion:{
+        join:"/users/join"
+      }
     },
-    status500: {
-      code:5000,
+    status1009: {
+      code: 1009,
+      status: 200,
+      message: "账号或密码错误!"
+    },
+    status1010: {
+      code: 1010,
+      status: 200,
+      message: "提交的内容存在格式错误!"
+    },
+    status1011: {
+      code: 1011,
       status: 500,
-      message: "server error!"
+      message: "登录失败!"
+    },
+    status1012: {
+      code: 1012,
+      status: 500,
+      message: "注册失败!"
+    },
+    status1013: {
+      code:1013 ,
+      status: 500,
+      message: "最近登录时间更新失败!"
+    }
+  },
+  tokenRes: {
+    status2000: {
+      code: 2000,
+      status: 200,
+      message: "成功!"
+    },
+    status2001: {
+      code: 2001,
+      status: 200,
+      message: "token已过期!"
+    },
+    status2002: {
+      code: 2002,
+      status: 200,
+      message: "token不存在!"
+    },
+    status2003: {
+      code: 2003,
+      status: 200,
+      message: "token无效!"
+    },
+    status2004: {
+      code: 2004,
+      status: 500,
+      message: "token移除失败!"
+    },
+    status2005: {
+      code: 2005,
+      status: 500,
+      message: "token添加失败!"
+    },
+    status2006: {
+      code: 2006,
+      status: 200,
+      message: "token移除成功!"
     }
   }
 }
--- local_modules/routes/users.js
+++ local_modules/routes/users.js
@@ -3,11 +3,13 @@
 //===============================================//
 var express = require('express'),
   bodyParser = require('body-parser'),
+  uuid = require('uuid'),
+  validator = require('validator'),
+  md5 = require('md5'),
   SqlOperation = require('../mongodb/sqloperation.js'),
-  config = require('../config/config.js'),
-  uuid = require('uuid');
-var router = express.Router();
-var SqlOperation = new SqlOperation();
+  config = require('../config/config.js');
+var router = express.Router(),
+  SqlOperation = new SqlOperation();
 //==================================================//
 //====================middleware====================//
 //==================================================//
@@ -18,12 +20,139 @@ router.use(bodyParser.urlencoded({ // to support URL-encoded bodies
 //====================================================//
 //====================router statr====================//
 //====================================================//
+
+//用户登录
+router.post('/login', function(req, res) {
+  var loginInfo = {
+      username: req.body.username,
+      password: req.body.password
+    }
+    //登录信息格式校验
+    //用户名不能为空，5-10个数字或英文字符
+    //密码不能为空，6-15个数字或英文字符
+  var check1 = validator.isAlphanumeric(loginInfo.username);
+  //var check2 = validator.isAlphanumeric(loginInfo.password);
+  var check3 = validator.isLength(loginInfo.username, 5, 10);
+  var check4 = validator.isLength(loginInfo.password, 6, 15);
+
+  if (check1 && check3 && check4) {
+    SqlOperation.findSpecify('users', {
+      username: loginInfo.username
+    }, function(results) {
+      if (results) {
+        if (results.password == md5(loginInfo.password)) {
+          var userInfo = results;
+          //更新用户最近登录时间,非阻塞
+          SqlOperation.update('users', {
+            username: userInfo.username
+          }, {
+            "$set": {
+              recently_time: Date.now() / 1000
+            }
+          }, function(results) {
+            console.log("登录时间更新结果");
+            console.log(results);
+          });
+          //移除旧的token并创建新的token
+          SqlOperation.removeMany('tokens', {
+            user_id: userInfo._id
+          }, function(results) {
+            console.log("token移除结果");
+            console.log(results);
+            if (results.result.ok == 1) {
+              //为登陆成功的用户创建新的token
+              var token = uuid.v4();
+              SqlOperation.insert('tokens', {
+                user_id: userInfo._id,
+                token: token,
+                create_time: Date.now() / 1000,
+                delete_time: Date.now() / 1000 + (86400 * 7)
+              }, function(results) {
+                console.log("token添加效果");
+                console.log(results);
+                if (results.result.ok == 1) {
+                  config.usersRes.status1000.data = userInfo;
+                  config.usersRes.status1000.token = token;
+                  res.status(200).send(config.usersRes.status1000);
+                } else {
+                  console.log("token插入失败");
+                  res.status(200).send(config.tokenRes.status2005);
+                }
+              })
+            }
+          });
+        } else {
+          console.log("密码错误");
+          res.status(200).json(config.usersRes.status1007);
+        }
+      } else {
+        console.log("账号不存在");
+        res.status(200).json(config.usersRes.status1008);
+      }
+    });
+  } else {
+    console.log("格式不正确");
+    console.log("username:" + check1 + " username5-10:" + check3 + " password6-16:" + check4);
+    res.status(200).json(config.usersRes.status1010);
+  }
+});
+
+//用户注册
+router.post('/join', function(req, res) {
+  //注册信息
+  var joinInfo = {
+    username: req.body.username,
+    email: req.body.email,
+    password: md5(req.body.password),
+    register_time: Date.now() / 1000,
+    role: 1,
+    level: 0,
+    status: 1
+  };
+  //注册信息格式校验
+  //用户名不能为空，5-10个数字或英文字符
+  //密码不能为空，32个数字或英文字符
+  var check1 = validator.isAlphanumeric(joinInfo.username);
+  //var check2 = validator.isAlphanumeric(joinInfo.password);
+  var check3 = validator.isEmail(joinInfo.email);
+  var check4 = validator.isLength(joinInfo.username, 5, 10);
+  var check5 = validator.isLength(req.body.password, 6, 15);
+  if (check1 && check3 && check4 && check5) {
+    //检查用户是否存在
+    SqlOperation.findSpecify('users', {
+      username: req.body.username
+    }, function(results) {
+      console.log(results);
+      if (results) {
+        res.status(200).send(config.usersRes.status1005);
+      } else {
+        //注册用户
+        SqlOperation.insert('users', joinInfo, function(result) {
+          console.log(result);
+          if (result.result.ok == 1) {
+            res.status(200).send(config.usersRes.status1000);
+          }
+        });
+      }
+    });
+  } else {
+    console.log("注册格式不对");
+    console.log("username:" + check1 + " email:" + check3 + " username5-10:" + check4 + " password6-16:" + check5);
+    res.status(200).json(config.usersRes.status1010);
+  }
+});
+//获得所有用户信息
+router.get('/', function(req, res, value) {
+  SqlOperation.findAll('users', function(results) {
+    config.usersRes.status1000.data = results;
+    res.status(200).json(config.usersRes.status1000);
+  });
+});
 //通过用户ID和token获取用户信息
 router.get('/:id', function(req, res) {
   var userId = SqlOperation.ObjectID(req.params.id);
   var token = req.query.token;
   var nowTime = Date.now() / 1000;
-
   SqlOperation.findSpecify('tokens', {
     user_id: userId
   }, function(results) {
@@ -31,137 +160,36 @@ router.get('/:id', function(req, res) {
       console.log("token返回结果");
       console.log(results);
       if (results.user_id == req.params.id && results.token == token && nowTime <= results.delete_time) {
-
         SqlOperation.findSpecify('users', {
           _id: userId
         }, function(results) {
           if (results) {
-            config.responseResult.status200.data = results;
-            res.status(200).send(config.responseResult.status200);
+            config.usersRes.status1000.data = results;
+            res.status(200).send(config.usersRes.status1000);
           } else {
-            res.status(404).send(config.responseResult.status404);
+            res.status(200).send(config.usersRes.status1008);
           }
         });
       } else if (nowTime > results.deleteTime) {
         console.log("token过期");
-        //delete token
+        //删除token
         SqlOperation.removeOne('tokens', {
           token: token
         }, function(results) {
-
+          if (results.result.ok == 1) {
+            res.status(200).send(config.tokenRes.status2006);
+          } else {
+            res.status(200).send(config.tokenRes.status2004);
+          }
         });
-        res.status(200).send(config.responseResult.status404);
+
       } else {
         console.log("token无效");
-        res.status(200).send(config.responseResult.status404);
+        res.status(200).send(config.tokenRes.status2003);
       }
     } else {
-      console.log("未查询到token");
-      res.status(200).send(config.responseResult.status404);
-    }
-
-  });
-});
-//用户登录
-router.post('/login', function(req, res) {
-  var loginInfo = {
-    username: req.body.username,
-    password: req.body.password
-  }
-  //登录信息格式校验
-  //......//
-  SqlOperation.findSpecify('users', {
-    username: loginInfo.username
-  }, function(results) {
-    if (results) {
-      if (results.password == loginInfo.password) {
-        var userInfo = results;
-        //移除旧的token
-        SqlOperation.removeMany('tokens', {
-          user_id: results._id
-        }, function(results) {
-          console.log("token移除结果");
-          //console.log(results);
-        });
-        //为登陆成功的用户创建新的token
-        var token = uuid.v4();
-        SqlOperation.insert('tokens', {
-          user_id: results._id,
-          token: token,
-          create_time: Date.now() / 1000,
-          delete_time: Date.now() / 1000 + (86400 * 7)
-        }, function(results) {
-          console.log("token添加效果");
-          console.log(results);
-          if (results.result.ok == 1) {
-            config.responseResult.status200.data = userInfo;
-            config.responseResult.status200.token = token;
-            //更新用户最近登录时间
-            SqlOperation.update('users', {
-              username: userInfo.username
-            }, {
-              "$set": {
-                recently_time: Date.now() / 1000
-              }
-            }, function(results) {
-              console.log("登录时间更新结果");
-              console.log(results);
-            })
-            res.status(200).send(config.responseResult.status200);
-          } else {
-            console.log("token插入失败");
-            res.status(200).send(config.responseResult.status404);
-          }
-        })
-      } else {
-        console.log("密码错误");
-        res.status(200).json(config.responseResult.status404);
-      }
-    } else {
-      console.log("账号不存在");
-      res.status(200).json(config.responseResult.status404);
-    }
-  });
-});
-//获得所有用户信息
-router.get('/', function(req, res, value) {
-  SqlOperation.findAll('users', function(results) {
-    config.responseResult.status200.data = results;
-    res.status(200).json(config.responseResult.status200);
-  });
-});
-//用户注册
-router.post('/join', function(req, res) {
-
-
-  //注册信息
-  var joinInfo = {
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-    register_time: Date.now() / 1000,
-    role: 1,
-    level: 0,
-    status: 1
-  };
-  //req.body数据校验
-  //......//
-
-  //检查用户是否存在
-  SqlOperation.findSpecify('users', {
-    username: req.body.username
-  }, function(results) {
-    console.log(results);
-    if (results) {
-      res.status(200).send(config.responseResult.status201);
-    } else {
-      //注册用户
-      SqlOperation.insert('users', joinInfo, function(result) {
-        console.log(result);
-        if (result.result.ok == 1) {
-          res.status(200).send(config.responseResult.status200);
-        }
-      });
+      console.log("token不存在");
+      res.status(200).send(config.tokenRes.status2002);
     }
   });
 });
